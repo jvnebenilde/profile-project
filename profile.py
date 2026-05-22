f5d2d399235e3c301abee0cf0af0f3ac501d2d6c
June Ongpin <junebugged@copland.local>
2026-05-22 18:35:18 +0800

    Update profile with skills list and graduation calculation

--- profile.py
+++ profile.py
@@ -6,3 +6,10 @@ is_student = True   # Boolean tracking state/conditions
 print("Name: ", name)
 print("Age: ", age)
 print("Student Status: ", is_student)
+
+# Appending new collection structures and calculations
+skills = ["Python", "Git", "HTML"]  # List collection type
+years_to_graduation = 4 - 1.5       # Float calculation evaluation
+
+print("Skills: ", skills)
+print("years left to graduate: ", years_to_graduation)
