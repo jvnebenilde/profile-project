c75ba6ecbbba47c18ef15ec49e4977604ad21a15
June Ongpin <junebugged@copland.local>
2026-05-22 18:15:37 +0800

    Initial commit: Add basic profile variables

---
+++ profile.py
@@ -0,0 +1,8 @@
+# Profile Generator - Version 1.0
+name = "Alex"       # String tracking alphabetic values
+age = 19            # Integer tracking discrete values
+is_student = True   # Boolean tracking state/conditions
+
+print("Name: ", name)
+print("Age: ", age)
+print("Student Status: ", is_student)
